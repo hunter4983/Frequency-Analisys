f4e9a3aca10e38bd6115b5e5dfe1d067a650b82f
Paco <109136574+Vyachh@users.noreply.github.com>
2023-11-11 18:49:05 +0300

    added: FAlib.py (all functions), writing all texts in one file

--- FrequencyAnalisys.py
+++ FrequencyAnalisys.py
@@ -3,114 +3,51 @@ from collections import Counter
 import os
 import sys
 import nltk 
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords
+import FAlib 
 
-def read_file(path):
-    f = open(path, "r", encoding="utf-8")
-    return f.read()
+print("Loading first text...")
+text1 = FAlib.read_file(r"C:\Users\vyach\Documents\GitHub\Frequency-Analisys\Text Examples\1. 12 chairs.txt")
+print("Loading second text...")
+text2 = FAlib.read_file(r"C:\Users\vyach\Documents\GitHub\Frequency-Analisys\Text Examples\2. Moscow from dawn to dawn.txt")
+print("Loading third text...")
+text3 = FAlib.read_file(r"C:\Users\vyach\Documents\GitHub\Frequency-Analisys\Text Examples\3. A story about one sun.txt")
+print("Loading fourth text...")
+text4 = FAlib.read_file(r"C:\Users\vyach\Documents\GitHub\Frequency-Analisys\Text Examples\4. Dog's heart.txt")
 
-def freq_analyze(text):
-    string.punctuation += "…’„»«1234567890–-"
-    filter = string.punctuation
-    stop_wordsRU=(stopwords.words('russian')) 
-    stop_wordsEN=(stopwords.words('english'))
-    #nltk.download('stopwords') # Загрузка стоп-слов, если необходимо
-    words = [word for word in text.translate(str.maketrans("", "", filter)).lower().split()]
-    words = [word for word in words if word not in stop_wordsRU]
-    words = [word for word in words if word not in stop_wordsEN]
-    words = [word for word in words if words.count(word) >= 1]
-    return Counter(words)
+FAlib.print_chars([text1,text2,text3,text4])
+
+sents_info1 = FAlib.sents_analyze(text1)
+sents_info2 = FAlib.sents_analyze(text2)
+sents_info3 = FAlib.sents_analyze(text3)
+sents_info4 = FAlib.sents_analyze(text4)
+
+word_count1 = sum(sents_info1[0])
+word_count2 = sum(sents_info2[0])
+word_count3 = sum(sents_info3[0])
+word_count4 = sum(sents_info4[0])
+
+average1 = round(word_count1 / sents_info1[1], 2)
+average2 = round(word_count2 / sents_info2[1], 2)
+average3 = round(word_count3 / sents_info3[1], 2)
+average4 = round(word_count4 / sents_info4[1], 2)
+
+freqs1 = FAlib.freq_analyze(text1)
+freqs2 = FAlib.freq_analyze(text2)
+freqs3 = FAlib.freq_analyze(text3)
+freqs4 = FAlib.freq_analyze(text4)
+
+FAlib.analyze([freqs1,freqs2,freqs3,freqs4], [average1,average2,average3,average4])
+
+# text = FAlib.read_file("C:\Coding\Python\\tol.txt")
+
+# sents_info = FAlib.sents_analyze(text)
     
-# Анализ текста по предложениям
-def sents_analyze(text):
-    sentences = sent_tokenize(text) 
-    word_counts = []
-    for sentence in sentences:
-        words = word_tokenize(sentence)
-        word_counts.append(len(words))
-    return word_counts, len(sentences)
 
-# Разбитие текста на предложения
-def sent_tokenize(text):
-    sentences = []
-    sentence = ""
-    for char in text:
-        sentence += char
-        if check_mark(char):
-            sentences.append(sentence)
-            sentence = ""
-    return sentences
-
-# Проверка символа is mark? 
-def check_mark(char):
-    if char in ['?', '!', '.']:
-        return True
-    return False
-
-# Разбитие текста на слова
-def word_tokenize(sentence):
-    chars = ""
-    words = []
-    for char in sentence:
-        chars += char
-        if char == " " or check_mark(char):
-            words.append(chars.strip())
-            chars = ""
-
-    return words
-
-
-# Пример текста для анализа
-text = read_file("C:\Coding\Python\\tol.txt")
-
-# ������ ���������� ���� � ������������
-sents_info = sents_analyze(text)
+# word_count = sum(sents_info[0])
     
-# Вычисляем количество слов в предложениях
-# for words in sents_info[0]:
-#         word_count += words
-word_count = sum(sents_info[0])
-    
-# Вычисляем среднее значение слов в каждом предложении
-average = round(word_count / sents_info[1], 2)
+# average = round(word_count / sents_info[1], 2)
 
-# Вызываем функцию частотного анализа и выводим результат
-freqs = freq_analyze(text)
+# freqs = FAlib.freq_analyze(text)
 
 
-#####################################################################################
-
-# ���������� ���������� ������� �� �������� �������
-sorted_freqs = dict(sorted(freqs.items(), key=lambda x: (-x[1], x[0])))
-
-# ��������� ���� ��� ������ ������
-sys.stdout = open("C:\Coding\Python\FrequencyAnalisys v.2\\result_filteredFreq.txt", "w", encoding="utf-8")
-
-# ������� ������� ���������� ���� � ������������
-print(f"Среднее значение слов в каждом предложении:{average}")
-
-# ������� ��������� ��� ������ ������ ���� � ������
-print("Частоты слов в тексте:")
-
-# ������� ��������� �����
-i = 0
-x = 0
-for word, freqs in sorted_freqs.items():
-    if freqs != x:
-        x = freqs
-        if x == freqs:
-            i = 0
-            print("")
-        print(f"{freqs}:\n{word}, ",end="")
-    else:
-        if i == 9:
-            print("")
-            i = 0
-        if freqs > 1:  # ��������� �����, ������� ����������� ������ 1 ���
-            print(f"{word}, ",end="")
-            i = i + 1
-
-# ��������� ���� ������
-sys.stdout.close()
-sys.stdout = sys.__stdout__  # ��������������� ����������� �����
-print("Done...")
