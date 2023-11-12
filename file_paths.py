969c9a43f3bcbe60d2349547e0bc15be09290093
hunter4983 <132560134+hunter4983@users.noreply.github.com>
2023-11-12 20:57:49 +0300

    hello
    path

--- file_paths.py
+++ file_paths.py
@@ -1,6 +1,6 @@
-default_path_files = r"C:\Vyacheslau\Coding\PythonApplication1\PythonApplication1\docs\\"
-default_path_results = r"C:\Vyacheslau\Coding\PythonApplication1\PythonApplication1\results\\"
-default_path_figures =r"C:\Vyacheslau\Coding\PythonApplication1\PythonApplication1\results\figures\\"
+default_path_files = r"docs\\"
+default_path_results = r"results\\"
+default_path_figures =r"results\figures\\"
 
 file_1_read = default_path_files + "1. Роман И.Ильфа, Е.Петрова ”Двенадцать стульев”, главы 1 и 2 .txt"
 file_2_read = default_path_files + "2. Рассказ И.Ильфа “Москва от зари до зари” Рассказ И.Ильфа “Случай в конторе”.txt"
